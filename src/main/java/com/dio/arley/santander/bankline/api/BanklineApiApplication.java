4324045cd2de90027a30e8571dadf78de734fc00
Arley Novais do Nascimento <nascimentoarle@gmail.com>
2022-05-05 17:49:28 -0300

    Fix problems when running

--- src/main/java/com/dio/arley/santander/bankline/api/BanklineApiApplication.java
+++ src/main/java/com/dio/arley/santander/bankline/api/BanklineApiApplication.java
@@ -3,13 +3,14 @@ package com.dio.arley.santander.bankline.api;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
-@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class })
+@SpringBootApplication
+@EnableJpaRepositories("com.dio.arley.santander.bankline.api.repository")
 public class BanklineApiApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(BanklineApiApplication.class, args);
-		
 		
 	}
 }
--- src/main/java/com/dio/arley/santander/bankline/api/repository/CorrentistaRepository.java
+++ src/main/java/com/dio/arley/santander/bankline/api/repository/CorrentistaRepository.java
@@ -1,11 +1,8 @@
 package com.dio.arley.santander.bankline.api.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
 import com.dio.arley.santander.bankline.api.model.Correntista;
 
-@Repository
 public interface CorrentistaRepository extends JpaRepository<Correntista, Integer>{
-
+	
 }
